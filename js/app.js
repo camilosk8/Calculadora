daf1d8c220f4e0f1e9f11c9dd2f13425556dffe4
camilosk8 <camilosk8.rey@gmail.com>
2017-10-17 21:38:46 -0500

    17-10-2017
    Variables Numeros

--- js/app.js
+++ js/app.js
@@ -1,30 +1,81 @@
+window.onload=manejarClics;
 
+var cifra1="";
 
+function manejarClics() {
 
+	var mas =document.getElementById("mas");
+	mas.addEventListener("click",function() {
+								operacion("+");
+								});
+	var menos=document.getElementById("menos");
+	menos.addEventListener("click",function() {
+								operacion("-");
+								});
+	var por=document.getElementById("por");
+	por.addEventListener("click",function() {
+								operacion("*");
+								});
+	var division=document.getElementById("dividido");
+	division.addEventListener("click",function() {
+								operacion("/");
+								});
+	var numero1=document.getElementById("1");
+	numero1.addEventListener("click",function() {
+								operacion("1");
+								});
+	var numero2=document.getElementById("2");
+	numero2.addEventListener("click",function() {
+								operacion("2");
+								});
+	var numero3=document.getElementById("3");
+	numero3.addEventListener("click",function() {
+								operacion("3");
+								});
+	var numero4=document.getElementById("4");
+	numero4.addEventListener("click",function() {
+								operacion("4");
+								});
+	var numero5=document.getElementById("5");
+	numero5.addEventListener("click",function() {
+								operacion("5");
+								});
 
+	var numero6=document.getElementById("6");
+	numero6.addEventListener("click",function() {
+								operacion("6");
+								});
+	var numero7=document.getElementById("7");
+	numero7.addEventListener("click",function() {
+								operacion("7");
+								});
 
+	var numero8=document.getElementById("8");
+	numero8.addEventListener("click",function() {
+								operacion("8");
+								});
 
+	var numero9=document.getElementById("9");
+	numero9.addEventListener("click",function() {
+								operacion("9");
+								});
 
+	var numero0=document.getElementById("0");
+	numero0.addEventListener("click",function() {
+								operacion("0");
+								});
 
-var on = document.getElementById("on");
-var sign = document.getElementById("sign");
-var raiz = document.getElementById("raiz");
-var dividido = document.getElementById("dividido");
-var 7 = document.getElementById("7");
-var 8 = document.getElementById("8");
-var 9 = document.getElementById("9");
-var por = document.getElementById("por");
-var 4 = document.getElementById("4");
-var 5 = document.getElementById("5");
-var 6 = document.getElementById("6");
-var menos = document.getElementById("menos");
-var 1 = document.getElementById("1");
-var 2 = document.getElementById("2");
-var 3 = document.getElementById("3");
-var 0 = document.getElementById("0");
-var punto = document.getElementById("punto");
-var igual = document.getElementById("igual");
-var mas = document.getElementById("mas");
+	var igual=document.getElementById("igual");
+	igual.addEventListener("click",calcular);
+	
+
+	var punto=document.getElementById("punto");
+	punto.addEventListener("click",function() {
+								operacion(".");
+								});
+	var elimina=document.getElementById("on");
+	elimina.addEventListener("click",eliminar);
+
 
 function display(){
 	var display = document.getElementById("display");
