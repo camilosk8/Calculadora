59a2f6d82ea22dfae37ba494f4681920b4a746d7
camilosk8 <camilosk8.rey@gmail.com>
2017-10-18 19:57:08 -0500

    18-10-2017
    Calculadora primer intento

--- js/app.js
+++ js/app.js
@@ -75,53 +75,29 @@ function manejarClics() {
 								});
 	var elimina=document.getElementById("on");
 	elimina.addEventListener("click",eliminar);
+}
 
 
 function display(){
 	var display = document.getElementById("display");
-
 }
 
-//Eventos
-uno.onclick = function(e){
-	resultado.textContent = resultado.textContent + "1"
-}
-dos.onclick = function(e){
-	resultado.textContent = resultado.textContent + "2"
-}
-tres.onclick = function(e){
-	resultado.textContent = resultado.textContent + "3"
-}
-cuatro.onclick = function(e){
-	resultado.textContent = resultado.textContent + "4"
-}
-cinco.onclick = function(e){
-	resultado.textContent = resultado.textContent + "5"
-}
-seis.onclick = function(e){
-	resultado.textContent = resultado.textContent + "6"
-}
-siete.onclick = function(e){
-	resultado.textContent = resultado.textContent + "7"
-}
-ocho.onclick = function(e){
-	resultado.textContent = resultado.textContent + "8"
-}
-nueve.onclick = function(e){
-	resultado.textContent = resultado.textContent + "9"
-}
-cero.onclick = function(e){
-	resultado.textContent = resultado.textContent + "0"
+function operacion(num){
+		cifra1+=num;
+        var pan=document.getElementById("display");
+		pan.innerHTML=cifra1;
 }
 
-reset.onclick = function(e){
-	resetear();
-}
-suma.onclick = function(e){
-	resetear();
-}
-resta.onclick = function(e){
-	resetear();
+function calcular()	{
+	var rta=eval(cifra1);
+	var pan=document.getElementById("display");
+	var conDecimal = rta.toFixed(2);
+	console.log(conDecimal);
+	pan.innerHTML=conDecimal;
 }
 
-	
+function eliminar(){
+	var total= eval(cifra1="0")
+	var pan=document.getElementById("display");
+	pan.innerHTML=0;
+}
